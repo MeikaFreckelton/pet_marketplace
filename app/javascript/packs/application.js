814059e098311fd15cffe506bf63898ede236a3b
meika freckelton <meikafreckelton@gmail.com>
2020-07-23 12:48:56 +1000

    set up bootstrap and nav and homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+//= require rails-ujs
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require activestorage
+//= require turbolinks
+//= require_tree .
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
